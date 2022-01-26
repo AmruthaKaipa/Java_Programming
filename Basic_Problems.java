4f05b70b148e54c76e84d01eea272d393086325b
AmruthaKaipa <amruthareddykaipa@gmail.com>
2022-01-26 20:13:32 +0530

    Printing Hello World

---
+++ Basic_Problems.java
@@ -0,0 +1,12 @@
+
+public class Basic_Problems {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello World");
+
+
+    }
+
+
+}
---
+++ out/production/Java_Programming/README.md
@@ -0,0 +1,2 @@
+# Java_Programming
+Repository that contains all the Problems solved in Java
